9fb93498379ecd1c66c9331bd559816729fc73ce
Mhd Ali Houria <alihoria123456@gmail.com>
2023-01-20 08:26:34 +0300

    fix deploy error

--- components/Error.js
+++ components/Error.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Error() {
   return (
     <div className="error-container">
-      <h1>Error - 404</h1> <h3>We don't have the flights you're looking for</h3>
+      <h1>Error - 404</h1> <h3>We don&apos;t have the flights you&apos;re looking for</h3>
     </div>
   );
 }
